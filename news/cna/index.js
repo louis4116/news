50322c15e05d60488aa6a5d7fdff94ae9d2c0591
潘冠辰 <91081800+louis4116@users.noreply.github.com>
2023-09-06 14:05:58 +0800

    updated

--- news/cna/index.js
+++ news/cna/index.js
@@ -1,10 +1,14 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 const cnaScrap = async (id) => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--disable-infobars", "--disable-gpu"],
-    ignoreDefaultArgs: ["--enable-automation"],
+  const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
   });
 
   const page = await browser.newPage();
@@ -64,3 +68,71 @@ const cnaScrap = async (id) => {
 };
 
 module.exports = cnaScrap;
+
+// const puppeteer = require("puppeteer");
+// const puppeteerCore = require("puppeteer-core");
+
+// const cnaScrap = async (id) => {
+//   const browser = await puppeteer.launch({
+//     headless: "new",
+//     args: ["--disable-infobars", "--disable-gpu"],
+//     ignoreDefaultArgs: ["--enable-automation"],
+//   });
+
+//   const page = await browser.newPage();
+//   await page.goto(`https://www.cna.com.tw/list/${id}.aspx`, {
+//     waitUntil: "domcontentloaded",
+//   });
+
+//   let count = 0;
+//   let maxCount = 2;
+//   if (id !== "aall") {
+//     //點擊畫面中的按鈕，讓新聞可以繼續跑
+//     while (count <= maxCount) {
+//       await page.waitForTimeout(200);
+//       await page.click("#SiteContent_uiViewMoreBtn_Style3");
+//       count++;
+//     }
+//   }
+
+//   const result = await page.evaluate(() => {
+//     let data = [];
+
+//     let newsItem = document.querySelectorAll(".mainList li");
+//     newsItem.forEach((el, i) => {
+//       let imgElement = newsItem[i]
+//         ?.querySelector(".wrap img")
+//         ?.getAttribute("src");
+//       if (imgElement) {
+//         data.push({
+//           source: "cna",
+//           title: newsItem[i].querySelector(".listInfo h2 span").textContent,
+//           date: newsItem[i].querySelector(".listInfo div").textContent,
+//           url: newsItem[i].querySelector("a").getAttribute("href"),
+//           img:
+//             newsItem[i]?.querySelector(".wrap img")?.getAttribute("src") || "",
+//         });
+//       } else {
+//         data.push({
+//           category: "cna",
+//           title: newsItem[i].querySelector(".listInfo h2 span").textContent,
+//           date: newsItem[i].querySelector(".listInfo div").textContent,
+//           url: newsItem[i].querySelector("a").getAttribute("href"),
+//         });
+//       }
+//     });
+
+//     return data;
+//   });
+//   await page.waitForFunction(
+//     (result) => {
+//       return result && result.length >= 20;
+//     },
+//     {},
+//     result
+//   );
+//   await browser.close();
+//   return result;
+// };
+
+// module.exports = cnaScrap;
